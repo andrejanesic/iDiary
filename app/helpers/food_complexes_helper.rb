4cdf3b29deb8e332f87fa8631da0cb974e5bfde0
Andreja Nesic <office@andrejanesic.com>
2022-09-08 03:21:26 +0200

    minor fix to foods

--- app/helpers/food_complexes_helper.rb
+++ app/helpers/food_complexes_helper.rb
@@ -7,25 +7,28 @@ module FoodComplexesHelper
   def food_complex_user_permission(fc_id, u_id)
     # #TODO this should be changed so they can't manage everything,
     # only verify
-    return FoodComplexesHelper::PERMISSION_OWNERSHIP if User.find(u_id).role == 'admin'
-
-    return FoodComplexesHelper::PERMISSION_READONLY if FoodComplex.find(fc_id).public
-
-    # if actual owner
-    if FoodComplex.find(fc_id).user_id == u_id
+    if User.find(u_id).role == 'admin'
       FoodComplexesHelper::PERMISSION_OWNERSHIP
     else
-      PERMISSION_NOPERMISSION
+
+      return FoodComplexesHelper::PERMISSION_READONLY if FoodComplex.find(fc_id).public
+
+      # if actual owner
+      if FoodComplex.find(fc_id).user_id == u_id
+        FoodComplexesHelper::PERMISSION_OWNERSHIP
+      else
+        PERMISSION_NOPERMISSION
+      end
+      # TODO: sharing for food_complexes
+      # if not owner, check if shared
+      #     food_complex_shares = FoodComplexShare.where(food_complex_id: fc_id, user_id: u_id)
+      #     if food_complex_shares.empty?
+      #       FoodComplexesHelper::PERMISSION_NOPERMISSION
+      #     else
+      #       food_complex_shares[0].permission
+      #     end
+      #   end
     end
-    # TODO: sharing for food_complexes
-    # if not owner, check if shared
-    #     food_complex_shares = FoodComplexShare.where(food_complex_id: fc_id, user_id: u_id)
-    #     if food_complex_shares.empty?
-    #       FoodComplexesHelper::PERMISSION_NOPERMISSION
-    #     else
-    #       food_complex_shares[0].permission
-    #     end
-    #   end
   rescue StandardError => e
     FoodComplexesHelper::PERMISSION_NOPERMISSION
   end
--- app/helpers/food_simples_helper.rb
+++ app/helpers/food_simples_helper.rb
@@ -7,25 +7,28 @@ module FoodSimplesHelper
   def food_simple_user_permission(fs_id, u_id)
     # #TODO this should be changed so they can't manage everything,
     # only verify
-    return FoodSimplesHelper::PERMISSION_OWNERSHIP if User.find(u_id).role == 'admin'
-
-    return FoodSimplesHelper::PERMISSION_READONLY if FoodSimple.find(fs_id).public
-
-    # if actual owner
-    if FoodSimple.find(fs_id).user_id == u_id
+    if User.find(u_id).role == 'admin'
       FoodSimplesHelper::PERMISSION_OWNERSHIP
     else
-      PERMISSION_NOPERMISSION
+
+      return FoodSimplesHelper::PERMISSION_READONLY if FoodSimple.find(fs_id).public
+
+      # if actual owner
+      if FoodSimple.find(fs_id).user_id == u_id
+        FoodSimplesHelper::PERMISSION_OWNERSHIP
+      else
+        PERMISSION_NOPERMISSION
+      end
+      # TODO: sharing for food_simples
+      # if not owner, check if shared
+      #     food_simple_shares = FoodSimpleShare.where(food_simple_id: fs_id, user_id: u_id)
+      #     if food_simple_shares.empty?
+      #       FoodSimplesHelper::PERMISSION_NOPERMISSION
+      #     else
+      #       food_simple_shares[0].permission
+      #     end
+      #   end
     end
-    # TODO: sharing for food_simples
-    # if not owner, check if shared
-    #     food_simple_shares = FoodSimpleShare.where(food_simple_id: fs_id, user_id: u_id)
-    #     if food_simple_shares.empty?
-    #       FoodSimplesHelper::PERMISSION_NOPERMISSION
-    #     else
-    #       food_simple_shares[0].permission
-    #     end
-    #   end
   rescue StandardError => e
     FoodSimplesHelper::PERMISSION_NOPERMISSION
   end
