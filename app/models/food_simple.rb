6cde7fef8e42e31b752593b1c4e925f4122dda6a
Andreja Nesic <office@andrejanesic.com>
2022-09-08 03:00:43 +0200

    minor things, heroku done

--- app/models/food_simple.rb
+++ app/models/food_simple.rb
@@ -1,5 +1,6 @@
 class FoodSimple < ApplicationRecord
   belongs_to :user
+  #TODO add must have amount and kcal, protein, etc!
   validates :name, presence: true, length: { maximum: 120 }, uniqueness: true
   validates :description, length: { maximum: 1000 }
 end
